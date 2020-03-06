4cf3f3c6d28346cbd341db0a544b865eee3078ea
Hatemii <hatem.xhezairi96@gmail.com>
2020-03-06 12:56:50 +0100

    Import all components from Material-UI

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,13 +1,16 @@
 import React from "react"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import AppBar from "material-ui/AppBar"
+import TextField from "material-ui/TextField"
+import RaisedButton from "material-ui/RaisedButton"
+
+
 
 export default class FormUserDetails extends React.Component {
   render(){
     return(
       <div>
         <h2>Hello From FormUserDetails</h2>
-          <h2>Hello From FormUserDetails</h2>
-            <h2>Hello From FormUserDetails</h2>
-        
       </div>
     )
   }
